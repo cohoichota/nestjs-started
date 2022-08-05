16de1ac99298753b8306e3bd09e7f153c688c607
cohoichota <cohoichota@gmail.com>
2022-08-05 17:15:32 +0700

    first commit

---
+++ src/users/users.entity.ts
@@ -0,0 +1,33 @@
+import { Entity, Column, PrimaryGeneratedColumn, AfterInsert, AfterRemove, AfterUpdate, OneToMany } from 'typeorm'
+import { Report } from 'src/reports/reports.entity';
+
+
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    email: string;
+
+    @Column()
+    password: string;
+
+    @OneToMany(() => Report, (report) => report.user)
+    reports: Report[];
+
+    @AfterInsert()
+    logInsert() {
+        console.log('Inserted User with id', this.id)
+    }
+
+    @AfterUpdate()
+    logUpdate() {
+        console.log('Updated User with id', this.id)
+    }
+
+    @AfterRemove()
+    logRemove() {
+        console.log('Removed User with id', this.id)
+    }
+}
